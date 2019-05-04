ce2387bd2f128a494706b0bfaea6914516e89ce2
Nicos Fabro <nicosfabroc@gmail.com>
2019-05-04 12:29:12 +0200

    Made SSH connect/close and upload script

--- get_vehicles.py
+++ get_vehicles.py
@@ -1,6 +1,16 @@
 import sys
 import requests
 import json
+import os
+import paramiko
+
+SERVER = "home556586389.1and1-data.host"
+USER = "u75998576-ecooltra"
+PASSWORD = "ecooltrahack"
+
+LOCAL_PATH = "./vehicles.json"
+REMOTE_PATH = "/vehicles.json"
+
 
 def get_vehicles():
 
@@ -22,8 +32,30 @@ def get_vehicles():
 
 	return 'Error'
 
+def connect_ssh_sftp():
+	ssh = paramiko.SSHClient() 
+	ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+	ssh.connect(SERVER, username=USER, password=PASSWORD)
+	sftp = ssh.open_sftp()
+
+	return ssh, sftp
+
+def close_ssh_sftp_close(sftp, ssh):
+	sftp.close()
+	ssh.close()
+
+def save_vehicles(sftp):
+	sftp.put(LOCAL_PATH, REMOTE_PATH)
+
 def main():
 	vehicles = get_vehicles()
+	with open(LOCAL_PATH, "w") as file_vehicles:
+		file_vehicles.write(vehicles)
+	
+	ssh, sftp = connect_ssh_sftp()
+	
+	save_vehicles(sftp)
+	close_ssh_sftp_close(sftp, ssh)
 
 if __name__ == "__main__":
 	main()
